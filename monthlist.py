ede386dbc4d6c51acddbccfcaff361316290959a
J.Ridgeway <sw333zy@gmail.com>
2019-04-08 13:19:30 -0400

    Create monthlist.py

---
+++ monthlist.py
@@ -0,0 +1,4 @@
+months=["jan", "feb", "march", "april", "may", "june", "july", "august", "sep", "oct", "nov", "dec"]
+for i in range(len(months)):
+    count=len(months[i])
+    print (months[i], count)
